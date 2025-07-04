892ed78613f531933aff39265f159b6580f50d62
usama aslam <usamadev241@gmail.com>
2025-07-04 22:16:30 +0500

    Inditial setup

---
+++ NewsAppAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsAppAssignment
+//
+//  Created by MacBook Pro on 04/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
